8e4696d5e7c5de7be8957fc1994cd59b60e0aea7
sananiki <157621575@qq.com>
2016-08-12 09:39:16 +0800

    'react'

---
+++ Head.js
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class AddTodo extends Component {
+
+
+  render() {
+    return (
+      <div>
+          <input type='text' ref='input' />
+      </div>
+    )
+  }
+
+}
